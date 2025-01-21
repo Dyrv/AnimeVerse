c78bbb61676de556c16f8d6c22ecc6214fe2a77e
Dyrv <diogofernandez8@gmail.com>
2025-01-21 14:46:36 +0000

    Update client.js

--- src/config/client.js
+++ src/config/client.js
@@ -2,8 +2,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 // URL do Supabase e chave de acesso
-const supabaseUrl = 'https://oaihncazmnywmuanzcxr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9haWhuY2F6bW55d211YW56Y3hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMyNDMyNjgsImV4cCI6MjA0ODgxOTI2OH0.XHMYbLmu8QxHLrIla-Ydi2b0O-sTUYR5GJDAwtfEhFI'; // Substitua pela sua chave pública ou serviço
+const supabaseUrl = 'Substitua pela seu link da supabase';
+const supabaseKey = 'Substitua pela sua chave pública ou serviço';
 
 // Cria uma instância do cliente Supabase
 const supabase =  createClient(supabaseUrl,supabaseKey);
